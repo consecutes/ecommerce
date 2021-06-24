1e95eda7b1fffdb0aa076b794127514e29be9b0f
Chris <chris.glood3011@gmail.com>
2021-06-24 15:42:36 -0400

    Fixing hero buttons Background color

--- views/public/js/heroButtons.js
+++ views/public/js/heroButtons.js
@@ -9,13 +9,15 @@ const foodHeroBtn = document.getElementById("foodHeroBtn");
 // Tittle
 const productsTittle = document.getElementById("productsTittle");
 // The item currently active (all products button by default)
-const activeItem = document.getElementById("heroCategoriesList").getElementsByClassName("active")[0];
+let activeItem = document.getElementById("heroCategoriesList").getElementsByClassName("active")[0];
 // All products button
 allHeroBtn.addEventListener("click", () => {
 	addPagination();
 	cardGenerator(document.getElementById("productsArea"), "shirts")
 
 	productsTittle.innerHTML="<strong>Products</strong>"
+
+	activeItem = document.getElementById("heroCategoriesList").getElementsByClassName("active")[0];
 
 	activeItem.classList.remove("active", "list-group-item-dark");
 	allHeroBtn.classList.add("active", "list-group-item-dark");
@@ -27,6 +29,8 @@ shirtsHeroBtn.addEventListener("click", () => {
 
 	productsTittle.innerHTML="<strong>Shirts</strong>"
 
+	activeItem = document.getElementById("heroCategoriesList").getElementsByClassName("active")[0];
+
 	activeItem.classList.remove("active", "list-group-item-dark");
 	shirtsHeroBtn.classList.add("active", "list-group-item-dark");
 });
@@ -36,6 +40,8 @@ pantsHeroBtn.addEventListener("click", () => {
 	cardGenerator(document.getElementById("productsArea"), "pants")
 
 	productsTittle.innerHTML="<strong>Pants</strong>"
+
+	activeItem = document.getElementById("heroCategoriesList").getElementsByClassName("active")[0];
 
 	activeItem.classList.remove("active", "list-group-item-dark");
 	pantsHeroBtn.classList.add("active", "list-group-item-dark");
@@ -47,6 +53,8 @@ tecnologyHeroBtn.addEventListener("click", () => {
 
 	productsTittle.innerHTML="<strong>Tecnology</strong>"
 
+	activeItem = document.getElementById("heroCategoriesList").getElementsByClassName("active")[0];
+	
 	activeItem.classList.remove("active", "list-group-item-dark");
 	tecnologyHeroBtn.classList.add("active", "list-group-item-dark");
 });
