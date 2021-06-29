93cf84ae6c56cf743df329a1d201a8dbfcc17a4d
Chris <chris.glood3011@gmail.com>
2021-06-28 22:44:41 -0400

    Fixing bootstrap npm problems

---
+++ views/public/js/bootstrap.js
@@ -0,0 +1,3 @@
+import '../../../node_modules/jquery/dist/jquery.slim.min.js'
+import '../../../node_modules/popper.js/dist/popper.min.js'
+import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js'
