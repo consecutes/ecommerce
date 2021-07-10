1127121f584b3f6e2fb83d0a129e75e1efff221c
Chris <chris.glood3011@gmail.com>
2021-07-10 14:27:28 -0400

    Loader spinner and profile pic

--- views/public/js/scripts.js
+++ views/public/js/scripts.js
@@ -1,3 +1,8 @@
-window.addEventListener("load", () => {
+// Product loader
+let productsAndPagination = document.getElementById("productsAndPagination");
+let loader = document.getElementById("loader");
 
+window.addEventListener("load", () => {
+	productsAndPagination.classList.remove("d-none");
+	loader.classList.add("d-none");
 });
