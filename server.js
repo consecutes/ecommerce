d79addb6a25bb4c71273c81b667c565227187b11
Chris <chris.glood3011@gmail.com>
2021-07-21 21:58:14 -0400

    Navbar responsive

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 // Módulos
 const path = require('path');
+const bodyParser = require("body-parser")
 // Configuración
+app.use(bodyParser.urlencoded({extended:false}))
 app.set('port', 3000 || process.env.PORT); // Puerto
 	// Archivos estáticos
 app.use('/public', express.static(path.join(__dirname, 'views/public')));
@@ -13,6 +15,7 @@ app.set("views", path.join(__dirname, '/views/templates'));
 app.set('view engine', 'pug');
 // Routing
 app.use(require('./src/routes/index'));
+app.use(require('./src/routes/login'));
 app.use(require('./src/routes/test'));
 app.use(require('./src/routes/api'));
 // Iniciando servidor
--- src/routes/api.js
+++ src/routes/api.js
@@ -4,9 +4,7 @@ const router = Router();
 const products = require('../storage/products.json');
 
 router.get('/api/products', (req, res) => {
-
 	res.json(products);
-
 });
 
 module.exports = router;
